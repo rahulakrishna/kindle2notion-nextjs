47a51f878ecdb6e947415e8b450aaa6c52a1bd81
Rahul A. Krishna <rkbalu524@gmail.com>
2022-10-09 20:30:53 +0530

    view clippings in page

--- pages/api/validate-clippings.ts
+++ pages/api/validate-clippings.ts
@@ -1,14 +1,98 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
+type Clipping = {
+  title: string;
+  author: string;
+  location: string;
+  body: string;
 };
 
-export default function handler(
+type CleanedClipping = {
+  title: string;
+  author: string;
+  clippings: Array<string>;
+};
+
+type Data = {
+  name: string;
+  clippings: Array<Clipping>;
+  cleanedClippings: Array<CleanedClipping>;
+};
+
+const getHighlightedContent = (slug: string) => {
+  if (slug) {
+    const [, ...rest] = slug.split(":");
+    return slug.split(":")[2].slice(2);
+  }
+  return "";
+};
+
+const getTitleAndAuthor = (slug: string) => {
+  const title = slug.split("(")[0].trim();
+  if (slug.split("(").length <= 1) {
+    return { title, author: "" };
+  } else {
+    const author = slug.split("(")[1].split(")")[0];
+    return {
+      title,
+      author,
+    };
+  }
+};
+
+const convertClippingsArrayToObject = (clippings: Array<Clipping>) => {
+  return clippings.reduce((acc: Array<CleanedClipping>, clipping: Clipping) => {
+    if (acc.find((a) => a.title === clipping.title)) {
+      return acc.map((a) => {
+        if (a.title === clipping.title) {
+          return {
+            ...a,
+            clippings: [...a.clippings, clipping.body],
+          };
+        }
+        return a;
+      });
+    }
+    return [
+      ...acc,
+      {
+        title: clipping.title,
+        author: clipping.author,
+        clippings: [clipping.body],
+      },
+    ];
+  }, []);
+};
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log({ req });
-  res.status(200).json({ name: "John Doe" });
+  const { notionApiAuthToken, notionDatabaseID, clippingsFile } = req.body;
+  console.log({ clippingsFile });
+  const clippingsArray = clippingsFile
+    .split("==========")
+    .map((s: string) => {
+      const { title, author } = getTitleAndAuthor(s.split("\n")[1]);
+      console.log({ slug: s, newlinesplit: s.split("\n") });
+      const content = s.split("\n")[4];
+      return {
+        title,
+        author,
+        location: s.split("|")[1],
+        body: content,
+      };
+    })
+    .filter((c: Clipping) => !!c.body && c.body !== "");
+
+  const cleanedClippings = await convertClippingsArrayToObject(clippingsArray);
+
+  console.log({ cleanedClippings });
+
+  res.status(200).json({
+    name: "John Doe",
+    clippings: clippingsArray,
+    cleanedClippings,
+  });
 }
--- styles/form.styles.js
+++ styles/form.styles.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const Form = styled.form`
-  .input,
-  button {
+  .input {
     margin-bottom: 16px;
   }
 `;
