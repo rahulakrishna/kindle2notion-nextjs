9fa80ed3fc23d7b63be40d68911b9abee86485b2
Rahul A. Krishna <rkbalu524@gmail.com>
2022-11-22 10:56:07 +0530

    add semantic css as a <link>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Container } from "semantic-ui-react";
 import Navbar from "../components/navbar";
